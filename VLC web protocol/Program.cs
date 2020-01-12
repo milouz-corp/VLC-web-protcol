15b605c5d5259ee7699a249fbaa9cd914554f14d
Milouz <thithony@users.noreply.github.com>
2020-01-12 11:08:42 +0100

    adding code

---
+++ VLC web protocol/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VLC_web_protocol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lines = "";
+
+            try
+            {
+                if (Environment.GetCommandLineArgs().Length > 1)
+                {
+                    var vlcArgs = Environment.GetCommandLineArgs()[1].Substring(6);
+                    vlcArgs = vlcArgs.Replace("%20", " ");
+                    Process.Start(AppDomain.CurrentDomain.BaseDirectory + "vlc.exe", vlcArgs);
+                }
+                else
+                {
+
+                    try
+                    {
+                        lines += "Installing\n";
+                        var paths = new List<string>();
+                        paths.Add(Environment.ExpandEnvironmentVariables("%ProgramW6432%"));
+                        paths.Add(Environment.ExpandEnvironmentVariables("%ProgramFiles(x86)%"));
+
+                        var exeName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+                        var vlcFound = false;
+                        foreach (var path in paths)
+                        {
+                            var vlcPath = path + @"\VideoLAN\VLC\";
+
+                            if (File.Exists(vlcPath + "vlc.exe"))
+                            {
+                                vlcFound = true;
+                                if (!Equals(AppDomain.CurrentDomain.BaseDirectory, vlcPath))
+                                {
+                                    lines += "Copying to VLC Path : " + vlcPath + "\n";
+                                    File.Copy(AppDomain.CurrentDomain.BaseDirectory + exeName, vlcPath + exeName, true);
+
+                                    lines += "Registering Protocol\n";
+                                    URISchema.Register("vlc", vlcPath + exeName, "%Host%", Microsoft.Win32.Registry.LocalMachine);
+
+                                    lines += "Installation Successfull\n";
+                                }
+                                else
+                                {
+                                    lines += "This is already installed\n";
+                                }
+                                break;
+                            }
+                        }
+                        if (!vlcFound)
+                        {
+                            lines += "VLC not found\n";
+                        }
+
+                        MessageBox.Show(lines);
+
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        MessageBox.Show(lines + "ERROR : Restart with Administrator privileges to install");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(lines + ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ VLC web protocol/UISchema.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VLC_web_protocol
+{
+    class URISchema
+    {
+
+        public static bool Register(string protocol, string application, string arguments, RegistryKey registry)
+        {
+
+            RegistryKey cl = Registry.ClassesRoot.OpenSubKey(protocol);
+
+            try
+            {
+                RegistryKey r;
+                r = registry.OpenSubKey("SOFTWARE\\Classes\\" + protocol, true);
+                if (r == null)
+                    r = registry.CreateSubKey("SOFTWARE\\Classes\\" + protocol);
+                r.SetValue("", "URL: Protocol handled by VLC");
+                r.SetValue("URL Protocol", "");
+                r.SetValue("CustomUrlApplication", application);
+                r.SetValue("CustomUrlArguments", arguments);
+
+                r = registry.OpenSubKey("SOFTWARE\\Classes\\" + protocol + "\\DefaultIcon", true);
+                if (r == null)
+                    r = registry.CreateSubKey("SOFTWARE\\Classes\\" + protocol + "\\DefaultIcon");
+                r.SetValue("", application);
+
+                r = registry.OpenSubKey("SOFTWARE\\Classes\\" + protocol + "\\shell\\open\\command", true);
+                if (r == null)
+                    r = registry.CreateSubKey("SOFTWARE\\Classes\\" + protocol + "\\shell\\open\\command");
+
+                r.SetValue("", application + " \"%1\"");
+
+
+                // If 64-bit OS, also register in the 32-bit registry area. 
+                if (registry.OpenSubKey("SOFTWARE\\Wow6432Node\\Classes") != null)
+                {
+                    r = registry.OpenSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol, true);
+                    if (r == null)
+                        r = registry.CreateSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol);
+                    r.SetValue("", "URL: Protocol handled by CustomURL");
+                    r.SetValue("URL Protocol", "");
+                    r.SetValue("CustomUrlApplication", application);
+                    r.SetValue("CustomUrlArguments", arguments);
+
+                    r = registry.OpenSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol + "\\DefaultIcon", true);
+                    if (r == null)
+                        r = registry.CreateSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol + "\\DefaultIcon");
+                    r.SetValue("", application);
+
+                    r = registry.OpenSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol + "\\shell\\open\\command", true);
+                    if (r == null)
+                        r = registry.CreateSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol + "\\shell\\open\\command");
+
+                    r.SetValue("", application + " \"%1\"");
+
+                }
+
+                //whitelist scheme to prevent chrome confirmation
+                r = registry.OpenSubKey("SOFTWARE\\Policies\\Google\\Chrome\\URLWhitelist", true);
+                if (r == null)
+                    r = registry.CreateSubKey("SOFTWARE\\Policies\\Google\\Chrome\\URLWhitelist" );
+                r.SetValue("42", protocol + "://*");
+
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+
+        }
+    }
+}
